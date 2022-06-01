b9c03be7245c0bd4a965f99ce2de9b9e80f72482
Arif Nazir <arif.nazir@assaabloy.com>
2022-06-01 02:40:39 +0200

    removed scan button

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-scanButton.addEventListener("click", async () => {
-  log("User clicked scan button");
-
-  try {
-    const ndef = new NDEFReader();
-    await ndef.scan();
-    log("> Scan started");
-
-    ndef.addEventListener("readingerror", () => {
-      log("Argh! Cannot read data from the NFC tag. Try another one?");
-    });
-
-    ndef.addEventListener("reading", ({ message, serialNumber }) => {
-      log(`> Serial Number: ${serialNumber}`);
-      log(`> Records: (${message.records.length})`);
-    });
-  } catch (error) {
-    log("Argh! " + error);
-  }
-});
-
 function a2utf16(string) {
   let result = new Uint16Array(string.length);
   for (let i = 0; i < string.length; i++) {
@@ -29,40 +8,20 @@ function a2utf16(string) {
 
 writeButton.addEventListener("click", async () => {
   log("User clicked write button");
-const ndef = new NDEFReader();
-let ignoreRead = false;
 
-ndef.onreading = (event) => {
-  if (ignoreRead) {
-    return; // write pending, ignore read.
+  try {
+    const ndef = new NDEFWriter();
+    const textRecord = {
+      recordType: "text",
+      lang: "en",
+      encoding: "utf-16",
+      data: a2utf16("{'token':'12345', 'expiry':'20220605'}")
+    };
+    ndef.write({ records: [textRecord] });
+    log("> Message written");
+  } catch (error) {
+    log("Argh! " + error);
   }
-
-  console.log("We read a tag, but not during pending write!");
-};
-
-function write(data) {
-  ignoreRead = true;
-  return new Promise((resolve, reject) => {
-    ndef.addEventListener("reading", event => {
-      // Check if we want to write to this tag, or reject.
-      ndef.write(data).then(resolve, reject).finally(() => ignoreRead = false);
-    }, { once: true });
-  });
-}
-
-ndef.scan();
-try {
-  const textRecord = {
-    recordType: "text",
-    lang: "en",
-    encoding: "utf-16",
-    data: a2utf16("{'token':'12345', 'expiry':'20220605'}")
-  };
-  ndef.write({ records: [textRecord] });
-  console.log("We wrote to a tag!")
-} catch(err) {
-  console.error("Something went wrong", err);
-}
 });
 
 makeReadOnlyButton.addEventListener("click", async () => {
