d0d33f8b43efe402e770d177a12c69b3a9c14e1b
Arif Nazir <arif.nazir@assaabloy.com>
2022-06-01 11:40:35 +0200

    test

--- index.js
+++ index.js
@@ -13,53 +13,22 @@ scanButton.addEventListener("click", async () => {
     ndef.addEventListener("reading", ({ message, serialNumber }) => {
       log(`> Serial Number: ${serialNumber}`);
       log(`> Records: (${message.records.length})`);
-      log(`> Text: (${message.records[0]})`)
     });
   } catch (error) {
     log("Argh! " + error);
   }
 });
 
-
-function readTextRecord(record) {
-  console.assert(record.recordType === "text");
-  const textDecoder = new TextDecoder(record.encoding);
-  console.log(`Text: ${textDecoder.decode(record.data)} (${record.lang})`);
-}
-
 writeButton.addEventListener("click", async () => {
   log("User clicked write button");
 
-  function a2utf16(string) {
-    let result = new Uint16Array(string.length);
-    for (let i = 0; i < string.length; i++) {
-      result[i] = string.codePointAt(i);
-    }
-    return result;
-  }
-  
-  const textRecord = {
-    recordType: "text",
-    lang: "en",
-    encoding: "utf-16",
-    data: a2utf16("{'name':'Arif', 'token':'12345', 'expiry':'20220605', 'access-level':'assaabloy'}")
-  };
-  const ndef = new NDEFReader();
-await ndef.scan();
-ndef.onreading = async (event) => {
-  const decoder = new TextDecoder();
-  for (const record of event.message.records) {
-    log("Record type:  " + record.recordType);
-    log("MIME type:    " + record.mediaType);
-    log("=== data===\n" + decoder.decode(record.data));
-  }
-
   try {
-    await ndef.write({ records: [textRecord] });
-  } catch(error) {
-    log(`Write failed :-( try again: ${error}.`);
+    const ndef = new NDEFReader();
+    await ndef.write("{'name':'Arif', 'token':'12345', 'expiry':'20220605', 'access-level':'assaabloy'}");
+    log("> Message written");
+  } catch (error) {
+    log("Argh! " + error);
   }
-};
 });
 
 makeReadOnlyButton.addEventListener("click", async () => {
